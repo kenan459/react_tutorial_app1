7f28dacdcbc6bd0cd37acd896275013f2ae6ebea
kenan-459 <kenanserran@gmail.com>
2024-10-02 22:57:09 +0700

    Creating Links (v5&v6)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
+import AboutIconLink from "./components/AboutIconLink";
 import AboutPage from "./pages/AboutPage";
 
 function App() {
@@ -27,17 +28,24 @@ function App() {
       <Header />
       <div className="container">
         <Routes>
-          <Route exact path='/' element= {
-            <>
-            <FeedbackForm handleAdd={addFeedback} />
-            <FeedbackStats feedback={feedback} />
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-            </>
-          }
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
           ></Route>
 
-          <Route path='/about' component={<AboutPage />}>
-          </Routes>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
       </div>
     </Router>
   );
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom";
+import Card from "../components/shared/card";
 
 export default function AboutPage() {
   return (
-    <div>AboutPage</div>
-  )
+    <Card>
+      <div className="about">
+        <h1>About Page</h1>
+        <p>This is a React app to leave feedback for a product or service.</p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+          <Link to="/">Back to Home</Link>
+        </p>
+      </div>
+    </Card>
+  );
 }
