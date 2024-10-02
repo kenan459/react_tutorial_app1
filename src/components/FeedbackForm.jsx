050c3b90aacea2ee40395d5b2795b0f0090da31f
kenan-459 <kenanserran@gmail.com>
2024-10-02 13:24:12 +0700

    Fade Animation With Framer Motion

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,20 +1,45 @@
 import { useState } from "react";
 import Card from "./shared/card";
-import Button from './shared/Button';
+import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
-export default function FeedbackForm() {
+export default function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState("10");
+  const [btnDisabled, setBtnDisabled] = useState("true");
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length < 10) {
+      setMessage("Text must be atleast 10 characters");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text: text,
+        rating: rating,
+      };
+      handleAdd(newFeedback);
+      setText('')
+    }
   };
 
   return (
     <div>
       <Card>
-        <form>
+        <form onSubmit={handleSubmit}>
           <h2>Rate your service with us:</h2>
-          {/* Rating select component */}
+          <RatingSelect select={(rating) => setRating(rating)} />
           <div className="input-group">
             <input
               onChange={handleTextChange}
@@ -22,8 +47,11 @@ export default function FeedbackForm() {
               placeholder="Write a review"
               value={text}
             />
-            <Button type='submit' version='secondary'>Send</Button>
+            <Button type="submit" version="secondary" isDisabled={btnDisabled}>
+              Send
+            </Button>
           </div>
+          {message && <div className="message">{message}</div>}
         </form>
       </Card>
     </div>
